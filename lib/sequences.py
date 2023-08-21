f34ef811fa3ac50ed5a7d90ca2bf7cde9ab3a3e1
Hiroki Kato <hirokikato1@gmail.com>
2023-08-21 08:48:57 -0700

    defines non-recursive method to print fibonacci sequence up to a given number--taken as argument

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    #do not do anything if length is less than 0
+    if length > 0:
+    #if length is any value greater than 0, append 0 => [0]
+        fibonacci_list.append(0)
+    if length >= 2:
+    #if length is greater than two, append 1, => [0,1]
+        fibonacci_list.append(1)
+        #iterate through all numbers starting at two ending at length
+        #append the sum of the last number + the second to last number
+        for num in range(2, length):
+            fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+
+    print(fibonacci_list)
+        
